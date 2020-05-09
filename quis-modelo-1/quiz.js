2512657ece8779e80f5a5bb3d92b56215f7a4998
Hatatori Nitsukawa <kcireh.i@gmail.com>
2020-05-09 19:14:15 -0300

    Add files via upload

--- quis-modelo-1/quiz.js
+++ quis-modelo-1/quiz.js
@@ -7,84 +7,123 @@ quest = qts.innerHTML.split("\n\n\n")
 questoes = []
 
 estrutura = {
-    pergunta: "alguma coisa",
-    enunciado: ['Certo','Errado'],
-    resposta: 0,
-    numero: 0,
-    marcado: 0,
-    rec : [],
+	pergunta: "alguma coisa",
+	enunciado: ['Certo','Errado'],
+	resposta: 0,
+	numero: 0,
+	marcado: 0,
+	rec : [],
 
-    items : function(){
-    	n = 0
-    	return this.enunciado.map(e=>"<label><input onclick='estrutura.gravar("+(n++)+","+this.numero+",this)' type='radio' name='q"+this.numero+"'><div></div>"+e+"</label>").join("")
-    },
+	items : function(){
+		n = 0
+		return this.enunciado.map(e=>"<label><input onclick='estrutura.gravar("+(n++)+","+this.numero+",this)' type='radio' name='q"+this.numero+"'><div></div>"+e+"</label>").join("")
+	},
 
-    gravar:function(e,a,c){
-    	this.rec[a] = e
-    	x = document.querySelector("#bt"+this.numero)
-    	x.setAttribute('res',e)
-
-    	document.querySelector("#bt"+a).setAttribute("res",e)
-
-    },
-
-
-    estrutura : function(){
-    	div = document.createElement("div")
-    	div.id = "e"+this.numero
-    	div.res = this.resposta
-    	div.innerHTML = "<p>"+this.pergunta+"</p>"
-    	div.innerHTML += this.items()
-    	div.innerHTML += "<div id='res' class='resposta'></div>"
-    	div.innerHTML += "<button v="+this.numero+" id=bt"+this.numero+" onclick='estrutura.verifica(this)' res="+this.resposta+">Verificar resposta</button>"
-    	return div
-    },
-
-    go : function(){
-    	ok.appendChild(this.estrutura())
-    },
-
-    verifica: function(a){
-    	
-    	posicao_botao = a.getAttribute('v') 
-    	resposta_botao = a.getAttribute('res') 
-
-    	console.log(posicao_botao-1)
+	gravar:function(e,a,c){
+		//posição da questão, número da questão, botao
+		this.rec[a] = e
+		x = document.querySelector("#bt"+this.numero)
+		x.setAttribute('res',e)
+		document.querySelector("#bt"+a).setAttribute("res",e)
+	},
 
 
-    	if(  resposta_botao == respostas[posicao_botao-1] ){
+	estrutura : function(){
+		div = document.createElement("div")
+		div.id = "e"+this.numero
+		div.res = this.resposta
+		div.innerHTML = "<p>"+this.pergunta+"</p>"
+		div.innerHTML += this.items()
+		div.innerHTML += "<div id='res' class='resposta'></div>"
+		div.innerHTML += "<button v="+this.numero+" id=bt"+this.numero+" onclick='estrutura.verifica(this)' res="+this.resposta+">Verificar resposta</button>"
+		return div
+	},
+
+	go : function(){
+		ok.appendChild(this.estrutura())
+	},
+
+	verifica: function(a){
+
+		posicao_botao = a.getAttribute('v') 
+		resposta_botao = a.getAttribute('res') 
+
+		console.log(posicao_botao-1)
+
+		tk = document.cookie.match(/lista=.+/g)[0].split("=")[1].split("")
+		tk[posicao_botao-1] = resposta_botao
+		tk = tk.join("")
+		document.cookie="lista="+tk
+
+
+		if(  resposta_botao == respostas[posicao_botao-1] ){
 			tag = a.parentElement.querySelector("#res")
-    		tag.innerHTML="Correto"
-    		
-    		tag.classList.remove("certo")
-    		tag.classList.remove("errado")
+			tag.innerHTML="Correto"
 
-    		tag.classList.toggle("certo")
+			tag.classList.remove("certo")
+			tag.classList.remove("errado")
+
+			tag.classList.toggle("certo")
 
 
-    	}else{
-    		tag = a.parentElement.querySelector("#res")
-    		tag.innerHTML="Inorreto"
+		}else{
+			tag = a.parentElement.querySelector("#res")
+			tag.innerHTML="Inorreto"
 
-    		tag.classList.remove("certo")
-    		tag.classList.remove("errado")
+			tag.classList.remove("certo")
+			tag.classList.remove("errado")
 
-    		tag.classList.toggle("errado")
-    	}
+			tag.classList.toggle("errado")
+		}
 
-    }
+	}
 }
-
-
-
-
-
 
 q = qts.innerHTML.split("\n\n\n")
 nume = 0
-for(i=0;i<q.length;i++){
+for(i=0;i<q.length;i++){	
 	estrutura.pergunta = nume+1+") "+q[i]
 	estrutura.numero++
 	estrutura.go()
 	nume++
 }
+
+//checar
+
+function checar_tudo(){
+	
+	z = document.cookie.match(/lista=.+/g)[0].split("=")[1]
+
+	console.log(z)
+
+	for(i=0;i<z.length;i++){
+
+		console.log(z[i])
+
+		el = document.querySelector("#e"+(i+1))
+
+		if(z[i] != "x"){
+			el.querySelectorAll('label')[z[i]].click()
+			el.querySelector('button').click()
+		}
+
+	}
+}
+
+
+//cookies
+
+// console.log(document.cookie.match(/lista/g))
+
+if(document.cookie.match(/lista/g) == null){
+
+	tx = ""
+	for(i=0;i<respostas.length;i++){
+		tx += "x"
+	}
+
+	document.cookie="lista="+tx
+	document.cookie.match(/lista=.+/g)[0].split("=")[1]
+}
+
+checar_tudo()
